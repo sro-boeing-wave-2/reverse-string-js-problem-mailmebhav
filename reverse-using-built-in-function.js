f7d175b7f63424c5352f195632ec4637b6ce1731
Bhavin Patel <mailmebhav@gmail.com>
2018-07-11 11:26:41 +0530

    methods chained in built-in function file

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,10 +1,8 @@
 // The intent of this file is to reverse
 // the string using built in functions.
 const reverse = (revstr) => {
-  const splitstr = revstr.split('');
-  const reversearr = splitstr.reverse();
-  const joinarr = reversearr.join('');
-  return joinarr;
+  const splitstr = revstr.split('').reverse().join('');
+  return splitstr;
 };
 
 module.exports = reverse;
